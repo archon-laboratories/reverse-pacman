3864cff03f43bb55f41d5f67e0ce6aff212068e3
Sam Beckmann <samvbeckmann@gmail.com>
2016-04-30 21:34:31 -0500

    Filled in Pacman class, included random implementation.

--- src/com/archonlaboratories/pacman/Pacman.java
+++ src/com/archonlaboratories/pacman/Pacman.java
@@ -2,19 +2,66 @@ package com.archonlaboratories.pacman;
 
 /**
  * Defines a pacman agent in the world.
+ *
+ * Abstract since the movement strategy should be
+ * left up to the implementation of the pacman.
  */
-public class Pacman
+public abstract class Pacman
 {
-    World.Tile location;
+    /**
+     * Keeps track of which tile pacman is currently in.
+     */
+    private World.Tile location;
+
+    public Pacman(World.Tile initLocation)
+    {
+        this.location = initLocation;
+    }
 
     /**
-     * Gets the next action from pacman.
+     * Get the current location of pacman.
+     * Note: Ghosts should not call this method.
      *
-     * @return the next action
+     * @return current Tile pacman is in.
      */
-    Action performAction()
+    public World.Tile getLocation()
     {
-        // TODO
-        return null;
+        return location;
+    }
+
+    /**
+     * Updates the location of pacman.
+     *
+     * @param location New Tile pacman is in.
+     */
+    public void setLocation(World.Tile location)
+    {
+        this.location = location;
+    }
+
+    /**
+     * Asks pacman for the next action he will perform.
+     * Location updating is handled by the Simulation.
+     *
+     * @return The action pacman will perform.
+     */
+    abstract Action performAction();
+
+    /**
+     * Implementation of Pacman where he moves randomly.
+     */
+    public class RandomPacman extends Pacman
+    {
+
+        public RandomPacman(World.Tile initLocation)
+        {
+            super(initLocation);
+        }
+
+        @Override
+        Action performAction()
+        {
+            return Action.randomAction();
+        }
     }
 }
