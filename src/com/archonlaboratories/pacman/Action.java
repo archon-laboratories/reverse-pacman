1ce2077d996e6baccb6099f0be416c87f6415ef9
Sam Beckmann <samvbeckmann@gmail.com>
2016-04-30 21:34:06 -0500

    Added random functionality to Action enum.

--- src/com/archonlaboratories/pacman/Action.java
+++ src/com/archonlaboratories/pacman/Action.java
@@ -1,9 +1,28 @@
 package com.archonlaboratories.pacman;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Defines the actions that can be taken by Pacman or the ghosts.
  */
 public enum Action
 {
-    UP, RIGHT, DOWN, LEFT
+    UP, RIGHT, DOWN, LEFT;
+
+    private static final List<Action> VALUES = Collections.unmodifiableList(Arrays.asList(values()));
+    private static final int SIZE = VALUES.size();
+    private static final Random RANDOM = new Random();
+
+    /**
+     * Returns a random value from this enum.
+     *
+     * @return A random Action
+     */
+    public static Action randomAction()
+    {
+        return VALUES.get(RANDOM.nextInt(SIZE));
+    }
 }
